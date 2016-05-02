277fa6319f3ce826acf3ce8bff3f86c5850444a8
DenisK2016 <denisov27111990@gmail.com>
2016-05-02 10:36:12 +0300

    Delete ProductsService.java

--- service/src/main/java/by/dk/training/items/service/ProductsService.java
+++
@@ -1,9 +0,0 @@
-package by.dk.training.items.service;
-
-
-import by.dk.training.items.datamodel.Products;
-import by.dk.training.items.datamodel.Types;
-
-public interface ProductsService {
-	void register(Products products, Types type);
-}
